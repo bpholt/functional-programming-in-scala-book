7bb80805eec5f0f3e4c26617f9c5c7c1905116ad
Brian P. Holt <bholt@planetholt.com>
2019-11-15 10:51:38 -0600

    update sum and product definitions per page 38

--- src/main/scala/fpinscala/datastructures/List.scala
+++ src/main/scala/fpinscala/datastructures/List.scala
@@ -9,12 +9,11 @@ case class Cons[+A](head: A, tail: List[A]) extends List[A]
 object List {
   def sum(ints: List[Int]): Int = ints match {
     case Nil => 0
-    case Cons(x,xs) => x + sum(xs)
+    case Cons(x, xs) => x + sum(xs)
   }
 
   def product(ds: List[Double]): Double = ds match {
     case Nil => 1.0
-    case Cons(0.0, _) => 0.0
     case Cons(x, xs) => x * product(xs)
   }
 
