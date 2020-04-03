f8ec0ae6f66d865089fffe92c999f5972fe98472
Brian P. Holt <bholt@planetholt.com>
2020-04-03 13:55:02 -0500

    checkpoint

--- src/main/scala/fpinscala/laziness/Stream.scala
+++ src/main/scala/fpinscala/laziness/Stream.scala
@@ -4,11 +4,11 @@ import scala.annotation.tailrec
 
 sealed trait Stream[+A] {
   def headOption: Option[A] =
-    this match {
-      case Empty => None
-      case Cons(h, _) => Some(h())
-    }
-//    foldRight()
+//    this match {
+//      case Empty => None
+//      case Cons(h, _) => Some(h())
+//    }
+    foldRight(None: Option[A])((a, _) => Some(a))
 
   def toList: List[A] = this match {
     case Empty => List.empty[A]
@@ -52,17 +52,57 @@ sealed trait Stream[+A] {
 //    }
     foldRight(true)((a, b) => p(a) && b)
 
-  def map[B](f: A => B): Stream[B] = ???
-  def filter(pred: A => Boolean): Stream[A] = ???
-  def append[AA >: A](as: Stream[AA]): Stream[AA] = ???
-  def flatMap[B](f: A => Stream[B]): Stream[B] = ???
+  def map[B](f: A => B): Stream[B] =
+    foldRight(Stream.empty[B]) { (a, bs) =>
+      Stream.cons(f(a), bs)
+    }
+
+  def filter(pred: A => Boolean): Stream[A] =
+    foldRight(Stream.empty[A]) { (a, b) =>
+      if (pred(a)) Stream.cons(a, b) else b
+    }
+
+  def append[AA >: A](as: Stream[AA]): Stream[AA] =
+    foldRight(as){ (a, b) =>
+      Stream.cons(a, b)
+    }
+
+  def prepend[AA >: A](as: Stream[AA]): Stream[AA] =
+    // cast is safe because all As are AAs too
+    as.foldRight(this.asInstanceOf[Stream[AA]]){ (a, b) =>
+      Stream.cons(a, b)
+    }
+
+  def flatMap[B](f: A => Stream[B]): Stream[B] =
+    foldRight(Stream.empty[B]) { (a, b) =>
+      f(a).append(b)
+    }
 }
 case object Empty extends Stream[Nothing]
 final case class Cons[+A](h: () => A, t: () => Stream[A]) extends Stream[A]
 
 object Stream {
   def onesPlus: Stream[Int] = Cons(() => 0, () => ones)
-  def ones: Stream[Int] = Cons(() => 1, () => ones)
+  def ones: Stream[Int] = constant(1) // Cons(() => 1, () => ones)
+
+  def constant[A](a: A): Stream[A] = {
+//    cons(a, constant(a))
+
+    // This is more efficient than `cons(a, constant(a))` since it's just
+    // one object referencing itself.
+    lazy val tail: Stream[A] = Cons(() => a, () => tail)
+    tail
+
+//    lazy val s: Stream[A] = cons(a, s)
+//    s
+  }
+
+  def from(n: Int): Stream[Int] =
+// TODO come back to this after exercise 5.13
+  //    constant(n).foldRight(ones) { (a, b) =>
+//      b.map(_ + a)
+//    }
+    cons(n, from(n + 1))
 
   def cons[A](hd: => A, tl: => Stream[A]): Stream[A] = {
     lazy val head = hd
@@ -73,4 +113,8 @@ object Stream {
   def apply[A](as: A*): Stream[A] =
     if (as.isEmpty) empty
     else cons(as.head, apply(as.tail: _*))
+}
+
+object Test extends App {
+  println(Stream.from(4).take(10).toList)
 }
