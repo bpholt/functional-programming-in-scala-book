38e2cdf5b260e715c121550dcf7c9fa44276ea3e
Brian P. Holt <bholt@planetholt.com>
2020-06-25 15:29:37 -0500

    re-implement during discussion

--- src/main/scala/fpinscala/state/State.scala
+++ src/main/scala/fpinscala/state/State.scala
@@ -1,5 +1,7 @@
 package fpinscala.state
 
+import cats.implicits._
+
 object State {
 
   def rollDieWithOffByOne: Int = {
@@ -14,12 +16,76 @@ trait RNG {
 }
 
 object RNG {
-  def nonNegativeInt(rng: RNG): (Int, RNG) = ???
-  def double(rng: RNG): (Double, RNG) = ???
-  def intDouble(rng: RNG): ((Int, Double), RNG) = ???
-  def doubleInt(rng: RNG): ((Double, Int), RNG) = ???
-  def double3(rng: RNG): ((Double, Double, Double), RNG) = ???
-  def ints(count: Int)(rng: RNG): (List[Int], RNG) = ???
+  def nonNegativeInt(rng: RNG): (Int, RNG) = {
+    val (i, r) = rng.nextInt
+
+    if (i < 0) nonNegativeInt(r) else (i, r)
+//    if (i < 0) (-i, r) else (i, r)
+//    (if(i < 0) -(i + 1) else i, r)
+  }
+
+  def double(rng: RNG): (Double, RNG) = {
+    val (i, r) = nonNegativeInt(rng)
+
+    if (i == Int.MaxValue) double(r) else (i.toDouble / Int.MaxValue, r)
+//    (i / (Int.MaxValue.toDouble + 1), r)
+
+//    if (i == 0) (0.0, r) else {
+//      (1.0 / i.toDouble, r)
+//    }
+  }
+  def intDouble(rng: RNG): ((Int, Double), RNG) = {
+    val (i, r) = rng.nextInt
+    val (d, r2) = double(r)
+
+    ((i, d), r2)
+  }
+  def doubleInt(rng: RNG): ((Double, Int), RNG) = {
+    val (d, r) = double(rng)
+    val (i, r2) = r.nextInt
+
+    ((d, i), r2)
+  }
+  def double3(rng: RNG): ((Double, Double, Double), RNG) = {
+    val (d1, r1) = double(rng)
+    val (d2, r2) = double(r1)
+    val (d3, r3) = double(r2)
+
+    ((d1, d2, d3), r3)
+  }
+  def ints(count: Int)(rng: RNG): (List[Int], RNG) = {
+//    val (l, r_out) = List.unfold((rng, count)) {
+//      case (_, c) if c <= 0 => None
+//      case (r, c) =>
+//        val (i, rn) = r.nextInt
+//
+//        Option(((i, rn), (rn, c - 1)))
+//    }.unzip
+//
+//    (l, r_out.last)
+
+    LazyList
+      .unfold(rng)(_.nextInt.pure[Option].map(t => (t, t._2)))
+      .take(count)
+      .toList
+      .unzip
+      .map(_.last)
+
+//    if (count == 0) (List.empty[Int], rng)
+//    else {
+////      val (tail, rt) = ints(count - 1)(rng)
+////      val (head, rh) = (rt).nextInt
+////
+////      (head :: tail, rh)
+//
+//      println("hello")
+//
+//      val (head, rh) = rng.nextInt
+//      val (tail, rt) = ints(count - 1)(rh)
+//
+//      (head :: tail, rt)
+//    }
+  }
 }
 
 case class SimpleRNG(seed: Long) extends RNG {
